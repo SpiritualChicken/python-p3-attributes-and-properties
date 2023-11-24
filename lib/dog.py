2e6b15954482de50e312208a5231820e4756f93b
Charlie <cnewlinds@gmail.com>
2023-11-24 23:32:51 +1100

    1 test pass

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,22 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name=""):
+        self._name = None
+        if name:
+            self.set_name(name)
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        if isinstance(value, str) and 0 < len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+
